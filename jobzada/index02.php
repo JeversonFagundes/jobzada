fc2ccaec1047f2584b0ca8e6859efba2a51e2a12
Jeverson Fagundes <jeverson.2022311922@aluno.iffar.edu.br>
2024-09-10 08:37:57 -0300

    jobzada

---
+++ jobzada/index02.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body class="brown  lighten-5">
+  <?php include_once "header.php" ?>
+
+  <main class="container">
+
+    <div style="margin-top: 10px;">
+      <div class="row">
+        <div class="col s12 m12  center-align"> <img class="responsive-img circle small" src="img/thiago.png" width="120"> <br> <b>Thiago Kassio Krug</b> </div>
+        <div class="col s12 m12  center-align"><br> Professor do IFfar e programador </div>
+      </div>
+
+      <div class="row">
+        <div class="col s12 m12  center-align">
+
+          <a class="btn-floating btn-small waves-effect waves-light black"><i class="material-icons">email</i></a>
+          <a class="btn-floating btn-small waves-effect waves-light black"><i class="material-icons">add_shopping_cart</i></a>
+          <a class="btn-floating btn-small waves-effect waves-light black"><i class="material-icons">card_giftcard</i></a></a>
+
+        </div>
+
+      </div>
+
+<div class="container"><hr>
+
+
+
+<br>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <div class="col s12  m3 center-align"> </div>
+    </div>
+    </div>
+
+    <div class="row">
+      <div class="col s12 m6 center-align "> <img class="responsive-img" src="img/img01.webp"></div>
+      <div class="col s12 m6 center-align "> <img class="responsive-img" src="img02.webp"> </div>
+    </div>
+
+    <div class="row">
+      <div class="col s12 m6 center-align"> <img class="responsive-img" src="img/img03.webp"> </div>
+      <div class="col s12 m6 center-align"> <img class="responsive-img" src="img/img04.webp"> </div>
+    </div>
+
+
+    <div class="row">
+      <div class="col s12 m6  center-align"> <img class="responsive-img" src="img/img03.webp"> </div>
+      <div class="col s12 m6 center-align"> <img class="responsive-img" src="img/img01.webp"> </div>
+    </div>
+
+
+
+
+
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+  </main>
+</body>
+
+</html>
